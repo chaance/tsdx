d11faf015f2adde481988c7f58052be1c7284550
Chance Strickland <chance.strickland@gmail.com>
2021-07-06 23:38:18 -0800

    update all deps, YOLO

--- src/templates/basic.ts
+++ src/templates/basic.ts
@@ -1,38 +1,46 @@
-import { Template } from './template';
+import { Template } from "./template";
+
+/* TODO: User husky v7
+husky: {
+      hooks: {
+        'pre-commit': 'tsdx lint',
+      },
+    },
+*/
 
 const basicTemplate: Template = {
-  name: 'basic',
-  dependencies: [
-    'husky',
-    'tsdx',
-    'tslib',
-    'typescript',
-    'size-limit',
-    '@size-limit/preset-small-lib',
-  ],
-  packageJson: {
-    // name: safeName,
-    version: '0.1.0',
-    license: 'MIT',
-    // author: author,
-    main: 'dist/index.js',
-    // module: `dist/${safeName}.esm.js`,
-    typings: `dist/index.d.ts`,
-    files: ['dist', 'src'],
-    engines: {
-      node: '>=10',
-    },
-    scripts: {
-      start: 'tsdx watch',
-      build: 'tsdx build',
-      test: 'tsdx test',
-      lint: 'tsdx lint',
-      prepare: 'tsdx build',
-      size: 'size-limit',
-      analyze: 'size-limit --why',
-    },
-    peerDependencies: {},
-    /*
+	name: "basic",
+	dependencies: [
+		"husky",
+		"tsdx",
+		"tslib",
+		"typescript",
+		"size-limit",
+		"@size-limit/preset-small-lib",
+	],
+	packageJson: {
+		// name: safeName,
+		version: "0.1.0",
+		license: "MIT",
+		// author: author,
+		main: "dist/index.js",
+		// module: `dist/${safeName}.esm.js`,
+		typings: `dist/index.d.ts`,
+		files: ["dist", "src"],
+		engines: {
+			node: ">=12",
+		},
+		scripts: {
+			start: "tsdx watch",
+			build: "tsdx build",
+			test: "tsdx test",
+			lint: "tsdx lint",
+			prepare: "tsdx build",
+			size: "size-limit",
+			analyze: "size-limit --why",
+		},
+		peerDependencies: {},
+		/*
     'size-limit': [
       {
         path: `dist/${safeName}.cjs.production.min.js`,
@@ -44,18 +52,12 @@ const basicTemplate: Template = {
       },
     ],
     */
-    husky: {
-      hooks: {
-        'pre-commit': 'tsdx lint',
-      },
-    },
-    prettier: {
-      printWidth: 80,
-      semi: true,
-      singleQuote: true,
-      trailingComma: 'es5',
-    },
-  },
+		// @ts-ignore
+		prettier: {
+			useTabs: true,
+			tabWidth: 2,
+		},
+	},
 };
 
 export default basicTemplate;
