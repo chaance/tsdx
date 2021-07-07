7427bab754adac1c3f8e87b03e5f7e58814039ce
Chance Strickland <chance.strickland@gmail.com>
2021-07-07 15:24:25 -0800

    formatting

--- templates/react/test/index.test.tsx
+++ templates/react/test/index.test.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import { Thing } from "../src";
 
 describe("Thing", () => {
+	// eslint-disable-next-line jest/expect-expect
 	it("renders without crashing", () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<Thing />, div);
