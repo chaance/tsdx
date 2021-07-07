f7a8a5fbd384165f6c09d84cf4b80d9f45a6b3ff
Chance Strickland <chance.strickland@gmail.com>
2021-07-07 12:45:41 -0800

    clean up unused razzle utils

--- src/messages.ts
+++ src/messages.ts
@@ -1,77 +1,55 @@
-import chalk from 'chalk';
-import getInstallCmd from './getInstallCmd';
-import * as Output from './output';
+import chalk from "chalk";
+import getInstallCmd from "./getInstallCmd";
+import * as Output from "./output";
 
-// This was copied from Razzle. Lots of unused stuff.
-const program = {
-  name: 'tsdx',
-};
-
-export const help = function() {
-  return `
-    Only ${chalk.green('<project-directory>')} is required.
+export function help() {
+	return `
+    Only ${chalk.green("<project-directory>")} is required.
     If you have any problems, do not hesitate to file an issue:
-    ${chalk.cyan('https://github.com/chaance/tsdx/issues/new')}
+    ${chalk.cyan("https://github.com/chaance/tsdx/issues/new")}
   `;
-};
+}
 
-export const missingProjectName = function() {
-  return `
-Please specify the project directory:
-  ${chalk.cyan(program.name)} ${chalk.green('<project-directory>')}
-For example:
-  ${chalk.cyan(program.name)} ${chalk.green('my-tsdx-lib')}
-Run ${chalk.cyan(`${program.name} --help`)} to see all options.
-`;
-};
+export function installing(packages: string[]) {
+	const pkgText = packages
+		.map(function (pkg) {
+			return `    ${chalk.cyan(chalk.bold(pkg))}`;
+		})
+		.join("\n");
 
-export const alreadyExists = function(projectName: string) {
-  return `
-Uh oh! Looks like there's already a directory called ${chalk.red(
-    projectName
-  )}. Please try a different name or delete that folder.`;
-};
-
-export const installing = function(packages: string[]) {
-  const pkgText = packages
-    .map(function(pkg) {
-      return `    ${chalk.cyan(chalk.bold(pkg))}`;
-    })
-    .join('\n');
-
-  return `Installing npm modules:
+	return `Installing npm modules:
 ${pkgText}
 `;
-};
+}
 
-export const installError = function(packages: string[]) {
-  const pkgText = packages
-    .map(function(pkg) {
-      return `${chalk.cyan(chalk.bold(pkg))}`;
-    })
-    .join(', ');
+export function installError(packages: string[]) {
+	const pkgText = packages
+		.map(function (pkg) {
+			return `${chalk.cyan(chalk.bold(pkg))}`;
+		})
+		.join(", ");
 
-  Output.error(`Failed to install ${pkgText}, try again.`);
-};
+	Output.error(`Failed to install ${pkgText}, try again.`);
+}
 
-export const copying = function(projectName: string) {
-  return `
+export function copying(projectName: string) {
+	return `
 Creating ${chalk.bold(chalk.green(projectName))}...
 `;
-};
+}
 
-export const start = async function(projectName: string) {
-  const cmd = await getInstallCmd();
+export async function start(projectName: string) {
+	const cmd = await getInstallCmd();
 
-  const commands = {
-    install: cmd === 'npm' ? 'npm install' : 'yarn install',
-    build: cmd === 'npm' ? 'npm run build' : 'yarn build',
-    start: cmd === 'npm' ? 'npm run start' : 'yarn start',
-    test: cmd === 'npm' ? 'npm test' : 'yarn test',
-  };
+	const commands = {
+		install: cmd === "npm" ? "npm install" : "yarn install",
+		build: cmd === "npm" ? "npm run build" : "yarn build",
+		start: cmd === "npm" ? "npm run start" : "yarn start",
+		test: cmd === "npm" ? "npm test" : "yarn test",
+	};
 
-  return `
-  ${chalk.green('Awesome!')} You're now ready to start coding.
+	return `
+  ${chalk.green("Awesome!")} You're now ready to start coding.
 
   I already ran ${Output.cmd(commands.install)} for you, so your next steps are:
     ${Output.cmd(`cd ${projectName}`)}
@@ -86,12 +64,12 @@ export const start = async function(projectName: string) {
     ${Output.cmd(commands.test)}
 
   Questions? Feedback? Please let me know!
-  ${chalk.green('https://github.com/chaance/tsdx/issues')}
+  ${chalk.green("https://github.com/chaance/tsdx/issues")}
 `;
-};
+}
 
-export const incorrectNodeVersion = function(requiredVersion: string) {
-  return `Unsupported Node version! Your current Node version (${chalk.red(
-    process.version
-  )}) does not satisfy the requirement of Node ${chalk.cyan(requiredVersion)}.`;
-};
+export function incorrectNodeVersion(requiredVersion: string) {
+	return `Unsupported Node version! Your current Node version (${chalk.red(
+		process.version
+	)}) does not satisfy the requirement of Node ${chalk.cyan(requiredVersion)}.`;
+}
