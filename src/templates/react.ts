b791626b5622bcdae142b7f7b0c88017ba50bad8
Chance Strickland <chance.strickland@gmail.com>
2021-07-07 12:44:50 -0800

    bump peerDep to react 17

--- src/templates/react.ts
+++ src/templates/react.ts
@@ -1,27 +1,26 @@
-import { Template } from './template';
-
-import basicTemplate from './basic';
-import { PackageJson } from 'type-fest';
+import basicTemplate from "./basic";
+import type { Template } from "./template";
+import type { PackageJson } from "type-fest";
 
 const reactTemplate: Template = {
-  name: 'react',
-  dependencies: [
-    ...basicTemplate.dependencies,
-    '@types/react',
-    '@types/react-dom',
-    'react',
-    'react-dom',
-  ],
-  packageJson: {
-    ...basicTemplate.packageJson,
-    peerDependencies: {
-      react: '>=16',
-    },
-    scripts: {
-      ...basicTemplate.packageJson.scripts,
-      test: 'tsdx test --passWithNoTests',
-    } as PackageJson['scripts'],
-  },
+	name: "react",
+	dependencies: [
+		...basicTemplate.dependencies,
+		"@types/react",
+		"@types/react-dom",
+		"react",
+		"react-dom",
+	],
+	packageJson: {
+		...basicTemplate.packageJson,
+		peerDependencies: {
+			react: ">=17",
+		},
+		scripts: {
+			...basicTemplate.packageJson.scripts,
+			test: "tsdx test --passWithNoTests",
+		} as PackageJson["scripts"],
+	},
 };
 
 export default reactTemplate;
